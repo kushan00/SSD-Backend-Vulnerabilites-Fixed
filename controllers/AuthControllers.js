ba16b9a4a5d3eb34f4b65c1a0bc704ace085de7b
Ramith <89182652+ramithperera@users.noreply.github.com>
2023-10-01 23:43:39 +0530

    change auth

--- controllers/AuthControllers.js
+++ controllers/AuthControllers.js
@@ -65,9 +65,9 @@ const registerUser = async (req, res) => {
     });
 
     //Encrypt Password
-    const salt = await bcrypt.genSalt(10);
+    // const salt = await bcrypt.genSalt(10);
 
-    user.password = await bcrypt.hash(password, salt);
+    // user.password = await bcrypt.hash(password, salt);
 
     await user.save();
 
